29172dd9792aa5bb7f3971ccad441d99503bc05c
Evan Jania <evanjania@gmail.com>
2026-02-15 17:46:10 -0500

    Add PHP backend API endpoints for login, signup, and color management

---
+++ api/test.php
@@ -0,0 +1,15 @@
+<?php
+phpinfo();
+?>
+```
+
+Save (Ctrl+X, Y, Enter)
+
+Now visit in your browser (or curl):
+```
+http://colorprojectevanjania.xyz/test.php
+```
+
+or 
+```
+http://209.38.67.201/test.php
