f1f73a083eea0f953b62611a9d34b01b58e693c3
Evan Jania <evanjania@gmail.com>
2026-04-03 13:41:21 -0400

    Refactor variable declarations to use const

--- public/js/code.js
+++ public/js/code.js
@@ -1,57 +1,49 @@
 const urlBase = 'http://colorprojectevanjania.xyz/LAMPAPI'
 const extension = 'php'
-
 let userId = 0
 let firstName = ''
 let lastName = ''
 
+// eslint-disable-next-line no-unused-vars
 function doLogin () {
   userId = 0
   firstName = ''
   lastName = ''
-
-  let login = document.getElementById('login-name').value
-  let password = document.getElementById('login-password').value
-
+  const login = document.getElementById('login-name').value
+  const password = document.getElementById('login-password').value
   document.getElementById('login-result').innerHTML = ''
-
-  let tmp = { login: login, password: password }
-  let jsonPayload = JSON.stringify(tmp)
-
-  let url = urlBase + '/Login.' + extension
-
+  const tmp = { login, password }
+  const jsonPayload = JSON.stringify(tmp)
+  const url = urlBase + '/Login.' + extension
   makeRequest(url, jsonPayload, 'login-result', function (response) {
-    let jsonObject = JSON.parse(response)
+    const jsonObject = JSON.parse(response)
     userId = jsonObject.id
-
     if (userId < 1) {
       document.getElementById('login-result').innerHTML = 'User/Password combination incorrect'
       return
     }
-
     firstName = jsonObject.firstName
     lastName = jsonObject.lastName
-
     saveCookie()
-
     window.location.href = 'color.html'
   })
 }
 
 function saveCookie () {
-  let minutes = 20
-  let date = new Date()
+  const minutes = 20
+  const date = new Date()
   date.setTime(date.getTime() + (minutes * 60 * 1000))
   document.cookie = 'firstName=' + firstName + ',lastName=' + lastName + ',userId=' + userId + ';expires=' + date.toGMTString()
 }
 
+// eslint-disable-next-line no-unused-vars
 function readCookie () {
   userId = -1
-  let data = document.cookie
-  let splits = data.split(',')
+  const data = document.cookie
+  const splits = data.split(',')
   for (let i = 0; i < splits.length; i++) {
-    let thisOne = splits[i].trim()
-    let tokens = thisOne.split('=')
+    const thisOne = splits[i].trim()
+    const tokens = thisOne.split('=')
     if (tokens[0] === 'firstName') {
       firstName = tokens[1]
     } else if (tokens[0] === 'lastName') {
@@ -60,12 +52,12 @@ function readCookie () {
       userId = parseInt(tokens[1].trim())
     }
   }
-
   if (userId < 0) {
     window.location.href = 'index.html'
   }
 }
 
+// eslint-disable-next-line no-unused-vars
 function doLogout () {
   userId = 0
   firstName = ''
@@ -75,7 +67,7 @@ function doLogout () {
 }
 
 function makeRequest (url, payload, resultElementId, successCallback) {
-  let xhr = new XMLHttpRequest()
+  const xhr = new XMLHttpRequest()
   xhr.open('POST', url, true)
   xhr.setRequestHeader('Content-type', 'application/json; charset=UTF-8')
   try {
@@ -90,41 +82,35 @@ function makeRequest (url, payload, resultElementId, successCallback) {
   }
 }
 
+// eslint-disable-next-line no-unused-vars
 function addColor () {
-  let newColor = document.getElementById('colorText').value
+  const newColor = document.getElementById('colorText').value
   document.getElementById('colorAddResult').innerHTML = ''
-
-  let tmp = { color: newColor, userId: userId }
-  let jsonPayload = JSON.stringify(tmp)
-
-  let url = urlBase + '/AddColor.' + extension
-
+  const tmp = { color: newColor, userId }
+  const jsonPayload = JSON.stringify(tmp)
+  const url = urlBase + '/AddColor.' + extension
   makeRequest(url, jsonPayload, 'colorAddResult', function (response) {
     document.getElementById('colorAddResult').innerHTML = 'Color has been added'
   })
 }
 
+// eslint-disable-next-line no-unused-vars
 function searchColor () {
-  let srch = document.getElementById('searchText').value
+  const srch = document.getElementById('searchText').value
   document.getElementById('colorSearchResult').innerHTML = ''
-
-  let tmp = { search: srch, userId: userId }
-  let jsonPayload = JSON.stringify(tmp)
-
-  let url = urlBase + '/SearchColors.' + extension
-
+  const tmp = { search: srch, userId }
+  const jsonPayload = JSON.stringify(tmp)
+  const url = urlBase + '/SearchColors.' + extension
   makeRequest(url, jsonPayload, 'colorSearchResult', function (response) {
-    document.getElementById('colorSearchResult').innerHTML = "Color(s) has been retrieved"
-    let jsonObject = JSON.parse(response)
+    document.getElementById('colorSearchResult').innerHTML = 'Color(s) has been retrieved'
+    const jsonObject = JSON.parse(response)
     let colorList = ''
-
     for (let i = 0; i < jsonObject.results.length; i++) {
       colorList += jsonObject.results[i]
       if (i < jsonObject.results.length - 1) {
         colorList += '<br />\r\n'
       }
     }
-
     document.getElementsByTagName('p')[0].innerHTML = colorList
   })
 }
